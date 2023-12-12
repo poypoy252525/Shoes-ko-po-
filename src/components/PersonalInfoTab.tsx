619a962037755f8cb844527ec1038de80dbeae39
delfin <carljefferson.delfin@gmail.com>
2023-12-12 20:54:25 +0800

    improve personal info update

--- src/components/PersonalInfoTab.tsx
+++ src/components/PersonalInfoTab.tsx
@@ -4,10 +4,15 @@ import useAuth from "../hooks/useAuth";
 import { Navigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import useUpdateCustomer from "../hooks/useUpdateCustomer";
+import useCustomer from "../hooks/useCustomer";
 
 const PersonalInfoTab = () => {
   const user = useAuth();
   if (!user) return <Navigate to="/login" />;
+
+  const { data: customer } = useCustomer(user.customer_id);
+
+  console.log(user);
 
   const { handleSubmit, setValue } = useForm<Customer>();
   const { mutate } = useUpdateCustomer();
@@ -32,7 +37,7 @@ const PersonalInfoTab = () => {
       <Grid templateColumns="repeat(2, 1fr)" gap={6}>
         <GridItem>
           <CustomInputFormControl
-            initialValue={user.first_name}
+            initialValue={customer?.first_name}
             setValue={(value) => setValue("first_name", value)}
             label="First name"
             inputType="text"
@@ -40,7 +45,7 @@ const PersonalInfoTab = () => {
         </GridItem>
         <GridItem>
           <CustomInputFormControl
-            initialValue={user.last_name}
+            initialValue={customer?.last_name}
             setValue={(value) => setValue("last_name", value)}
             label="Last name"
             inputType="text"
@@ -48,7 +53,7 @@ const PersonalInfoTab = () => {
         </GridItem>
         <GridItem>
           <CustomInputFormControl
-            initialValue={user.address}
+            initialValue={customer?.address}
             setValue={(value) => setValue("address", value)}
             label="Address"
             inputType="text"
@@ -56,7 +61,9 @@ const PersonalInfoTab = () => {
         </GridItem>
         <GridItem>
           <CustomInputFormControl
-            initialValue={user.phone_number ? user.phone_number.toString() : ""}
+            initialValue={
+              customer?.phone_number ? customer?.phone_number.toString() : ""
+            }
             setValue={(value) => setValue("phone_number", parseInt(value))}
             label="Phone number"
             inputType="number"
